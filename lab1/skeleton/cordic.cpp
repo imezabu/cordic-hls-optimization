a594b7f025a7f7c93bc5f545297df9b9360d2100
Isaac Meza <imeza@bu.edu>
2026-02-07 17:43:04 -0500

    Cordic Core

--- lab1/skeleton/cordic.cpp
+++ lab1/skeleton/cordic.cpp
@@ -18,17 +18,33 @@ void cordic(theta_type theta, cos_sin_type &s, cos_sin_type &c) {
 // -----------------------------
 // YOUR CODE GOES HERE
 // -----------------------------
-
+  cos_sin_type x = 0.60735;
+  cos_sin_type y = 0;
 #ifdef FIXED_TYPE // fixed-point design
 FIXED_STEP_LOOP:
   for (int step = 0; step < 20; step++) {
+    int sigma = (theta<0) ? -1:1;
+    cos_sin_type new_x = x-(sigma*(y>>step));
+    cos_sin_type new_y= y +(sigma*(x>>step));
+
+    x=new_x;
+    y=new_y;
+    theta=theta-sigma*cordic_ctab[step];
   }
 
 #else // floating point design
 
 FLOAT_STEP_LOOP:
   for (int step = 0; step < NUM_ITER; step++) {
+    int sigma = (theta<0) ? -1:1;
+    cos_sin_type new_x = x-(sigma*(ldexp(y,-step)));
+    cos_sin_type new_y= y +(sigma*(ldexp(x,-step)));
+
+    x=new_x;
+    y=new_y;
+    theta=theta-sigma*cordic_ctab[step];
   }
 
 #endif
+  c=x; s=y;
 }
