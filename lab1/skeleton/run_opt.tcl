e45e5b45486e61cbabcbcc9f64efa37feab7f6d6
Isaac Meza <imeza@bu.edu>
2026-02-10 01:23:28 -0500

    Implemented basic run functionality

--- lab1/skeleton/run_opt.tcl
+++ lab1/skeleton/run_opt.tcl
@@ -11,3 +11,62 @@
 # Your Vivado HLS project script goes here
 # HINT: You can use run_fixed.tcl as a template. Then, run this script with
 # `vivado_hls -f run_opt.tcl`
+
+set filename "opt_result.csv"
+file delete -force "./result/${filename}"
+
+#-----------------------------------------------------
+# You can specify a set of bitwidth configurations to 
+# explore in the batch experiments. 
+# Each configuration (line) is defined by a pair in  
+# total bitwidth and integer bitwidth
+# Examples are shown below. 
+#-----------------------------------------------------
+set bitwidth_pair { \
+    32    8   \
+}
+
+#-----------------------------------
+# run batch experiments
+#-----------------------------------
+foreach { TOT_W INT_W } $bitwidth_pair {
+
+# Define the bitwidth macros from CFLAGs
+set CFLAGS "-DFIXED_TYPE -DFULL_WIDTH=${TOT_W} -DI_WIDTH=${INT_W}"
+
+# Project name
+set hls_prj "opt_${TOT_W}_${INT_W}_20.prj"
+
+# Open/reset the project
+open_project ${hls_prj} -reset
+# Top function of the design is "cordic"
+set_top cordic
+
+# Add design and testbench files
+add_files cordic.cpp -cflags $CFLAGS
+add_files -tb cordic_test.cpp -cflags $CFLAGS
+
+open_solution "solution1" -flow_target vitis
+# use KV260 device (Kria K26 SOM - Zynq UltraScale+ MPSoC)
+set_part xck26-sfvc784-2LV-c
+
+# Target clock period is 10ns
+create_clock -period 10
+
+# Simulate the C++ design
+csim_design
+# Synthesize the design
+csynth_design
+
+# We will skip C-RTL cosimulation for now
+#cosim_design
+
+#---------------------------------------------
+# Collect & dump out results from HLS reports
+#---------------------------------------------
+set argv [list $filename $hls_prj]
+set argc 2
+source "./script/collect_result.tcl"
+}
+
+quit
