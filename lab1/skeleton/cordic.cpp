3c777beed7068a79a4adc001adb6f83095108ea2
imezabu <imeza@bu.edu>
2026-02-10 22:45:08 -0500

    Modified Code Body to help implement HLS optimizations

--- lab1/skeleton/cordic.cpp
+++ lab1/skeleton/cordic.cpp
@@ -18,11 +18,10 @@ void cordic(theta_type theta, cos_sin_type &s, cos_sin_type &c) {
 // -----------------------------
 // YOUR CODE GOES HERE
 // -----------------------------
-  
+  cos_sin_type x = 0.607252;
   cos_sin_type y = 0;
 #ifdef FIXED_TYPE // fixed-point design
 FIXED_STEP_LOOP:
-  cos_sin_type x = 0.607252; //adjusted for the asymptote of infinite iterations of CORDIC gain
   for (int step = 0; step < 20; step++) {
     cos_sin_type new_x = (theta<0) ? x+(y>>step) : x-(y>>step); //no multiplication in design
     cos_sin_type new_y= (theta<0)? y -(x>>step): y +(x>>step);
@@ -34,8 +33,7 @@ FIXED_STEP_LOOP:
 
 #else // floating point design
 
-FLOAT_STEP_LOOP:
-  cos_sin_type x = 0.607351; //adjusted for 5 iterations of CORDIC GAIN 
+FLOAT_STEP_LOOP: 
   for (int step = 0; step < NUM_ITER; step++) {
     int sigma = (theta<0) ? -1:1;
     cos_sin_type new_x = x-sigma*(y / (double)(1ULL << step));
